9dd48e4147296c6a2d4aa601b8d110d8a7fc4558
sondoan17 <sondoan17@gmail.com>
2025-09-22 04:15:50 +0700

    feat: implement budget module

---
+++ apps/api/src/budgets/dto/upsert-budget.dto.ts
@@ -0,0 +1,30 @@
+import { Currency } from "@prisma/client";
+import { Type } from "class-transformer";
+import { IsEnum, IsInt, IsNumber, IsOptional, IsPositive, IsString, Max, Min } from "class-validator";
+
+export class UpsertBudgetDto {
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(12)
+  month!: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(2000)
+  @Max(2100)
+  year!: number;
+
+  @Type(() => Number)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  limitAmount!: number;
+
+  @IsOptional()
+  @IsEnum(Currency)
+  currency?: Currency = Currency.VND;
+
+  @IsOptional()
+  @IsString()
+  categoryId?: string;
+}
