dcc5c18586f78b64c62bce1f6aea7bd8607ddf30
sondoan17 <sondoan17@gmail.com>
2025-09-22 04:12:03 +0700

    feat: import app's main modules

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { UsersModule } from "./users/users.module";
+import { TransactionsModule } from "./transactions/transactions.module";
+import { BudgetsModule } from "./budgets/budgets.module";
+import { ReportsModule } from "./reports/reports.module";
+import { AgentModule } from "./agent/agent.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    TransactionsModule,
+    BudgetsModule,
+    ReportsModule,
+    AgentModule,
+  ],
+})
+export class AppModule {}
