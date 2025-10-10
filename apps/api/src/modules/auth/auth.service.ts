ac6037227fb880589c1b216b3f129c7dedddd7a9
sondoan17 <32607149+sondoan17@users.noreply.github.com>
2025-10-10 13:02:49 +0700

    Update auth.service.ts

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -152,7 +152,7 @@ export class AuthService {
 
     res.cookie('access_token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'development',
+      secure: true,
       sameSite: 'none',
       maxAge,
       path: '/',
@@ -162,7 +162,7 @@ export class AuthService {
   clearAuthCookie(res: Response): void {
     res.clearCookie('access_token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'development',
+      secure: true,
       sameSite: 'none',
       path: '/',
     });
